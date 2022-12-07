765ba486b159fe33f6c26712ab98e31081d58f85
fifde <fifdee@outlook.com>
2022-12-07 16:38:04 +0100

    updated values for periodic task (leaderboards with no update in 31 days will be deleted), task launched every 4 hours

--- leaderboards/tasks.py
+++ leaderboards/tasks.py
@@ -7,9 +7,9 @@ from huey.contrib.djhuey import db_periodic_task, db_task
 from leaderboards.models import Leaderboard
 
 
-@db_periodic_task(crontab(minute='*/5'))
+@db_periodic_task(crontab(hour='*/4'))
 def leaderboard_expired():
     leaderboards = Leaderboard.objects.all()
     for leaderboard in leaderboards:
-        if now() - leaderboard.modified_date > datetime.timedelta(minutes=15):
+        if now() - leaderboard.modified_date > datetime.timedelta(days=31):
             leaderboard.delete()
