9bdf7d941df4868705709312302c1227d01321cd
fifde <fifdee@outlook.com>
2023-03-05 18:55:13 +0100

    Protection against 'username == None'

--- leaderboards/my_mixins.py
+++ leaderboards/my_mixins.py
@@ -7,15 +7,18 @@ class CheckIfUserConverted(generic.View):
     def dispatch(self, *args, **kwargs):
         user = self.request.user
         if user.is_authenticated and user.temporary:
-            temp_password_part = settings.TEMP_PASSWORD_PART
-            id_part = user.username.split('_')[1]
-            password = temp_password_part + '_' + id_part
+            try:
+                temp_password_part = settings.TEMP_PASSWORD_PART
+                id_part = user.username.split('_')[1]
+                password = temp_password_part + '_' + id_part
 
-            temporary = authenticate(self.request, username=user.username, password=password)
-            if not temporary:
-                # default credentials don't work - new password was set
-                print('not temporary')
-                user.temporary = False
-                user.save()
+                temporary = authenticate(self.request, username=user.username, password=password)
+                if not temporary:
+                    # default credentials don't work - new password was set
+                    print('not temporary')
+                    user.temporary = False
+                    user.save()
+            except IndexError:
+                ...
 
         return super(CheckIfUserConverted, self).dispatch(*args, **kwargs)
